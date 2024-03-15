888e55a2b4fc4e7e05cfa54b03252e34488e7c56
tuomi105 <tuomi105@umn.edu>
2024-03-15 09:47:45 -0500

    Updated
    Added comments and changed path destinations to reflect clone location

--- btcc.sh
+++ btcc.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
 BTCACHE_LOCATION=/var/lib/bluetooth/*
-LOG=/home/tdcs/btlog.txt
-RPW=fasd
+LOG=/home/tdcs/Bluetooth_fixer/btlog.txt
+RPW=tdcscog1
 date=$(date +'%m/%d/%y %H:%M:%S');
 
+##Removes any cached bluetooth devices; prevents a common error where you can not setup a disconnected device
 echo $RPW | su -c "cd $BTCACHE_LOCATION && rm -r cache && mkdir cache" root
+##Clears pagecache, dentries, inodes, and swap memory - may help if there is a caching error
 echo $RPW | su -c "echo 3 >'/proc/sys/vm/drop_caches' && swapoff -a && swapon -a" root
 
+##Prints whether or not the script was ran successfully and at what time.
 if [ $? -ne 0 ]; then
 	echo "Error occurred on $date" >> $LOG
 else
+##If succesful, log is overwritten with the date and time of the last clearing
 	echo "Bluetooth and RAM cache last cleared on $date" > $LOG 
 fi
