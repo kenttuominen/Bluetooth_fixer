fffea47729cb43cecfc8435d493c527c74a674cb
tuomi105 <tuomi105@umn.edu>
2024-03-15 12:07:14 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ PW=lnpicog1
 RPW=tdcscog1
 
 ##Make password consistent
-echo $PW | sudo sh -c 'echo root:$RPW | chpasswd'
+echo $PW | sudo sh -c 'echo root:tdcscog1 | chpasswd'
 
 ##Make scripts executable
 echo $PW | sudo chmod +x btcc.sh | echo $PW | sudo chmod +x btrem.sh
@@ -12,10 +12,10 @@ echo $PW | sudo chmod +x btcc.sh | echo $PW | sudo chmod +x btrem.sh
 echo $RPW | su -c "echo '@reboot root sh /home/tdcs/Bluetooth_fixer/btcc.sh' >> /etc/crontab" root
 
 ##Move desktop shortcut to applications folder
-cp bluetooth_reset.desktop /usr/share/applications/bluetooth_reset.desktop
+cp /home/tdcs/Bluetooth_fixer/bluetooth_reset.desktop /usr/share/applications/bluetooth_reset.desktop
 
-echo $PW | sudo gio set /usr/share/applications/bluetooth_reset.desktop metadata::trusted true
 echo $PW | sudo chmod a+x /usr/applications/bluetooth_reset.desktop
+echo $PW | sudo dbus-launch gio set /usr/share/applications/bluetooth_reset.desktop metadata::trusted true
 
 ##Move the shortcut to the desktop
 ##cp bluetooth_reset.desktop /home/tdcs/Desktop/bluetooth_reset.desktop
