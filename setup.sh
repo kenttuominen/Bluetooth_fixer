9af4077611d7f2cf258dff4435c6e8681a855418
tuomi105 <tuomi105@umn.edu>
2024-03-15 09:40:38 -0500

    Updated setup
    Added commenting and changed password to something more generic

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 PW=lnpicog1
-RPW=fasd
+RPW=tdcscog1
 
+##Make password consistent
+echo $PW | sudo sh -c 'echo root:$RPW | chpasswd'
+##Make scripts executable
 echo $PW | sudo chmod +x btcc.sh | echo $PW | sudo chmod +x btrem.sh
-echo $RPW | su -c "echo '@reboot    root sh /home/tdcs/btcc.sh' >> /etc/crontab"
-mv bluetooth_reset.desktop /home/tdcs/Desktop/bluetooth_reset.desktop
+##Add script to crontab to activate upon reboot
+echo $RPW | su -c "echo '@reboot root sh /home/tdcs/Bluetooth_fixer/btcc.sh' >> /etc/crontab" root
+##Move the shortcut to the desktop
+cp bluetooth_reset.desktop /home/tdcs/Desktop/bluetooth_reset.desktop
+##Make the shortcut launchable
 gio set /home/tdcs/Desktop/bluetooth_reset.desktop metadata::trusted true
 chmod a+x /home/tdcs/Desktop/bluetooth_reset.desktop
